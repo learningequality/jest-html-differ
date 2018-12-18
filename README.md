b14e20f5f20295f4805f929d6c682775fd4a34a1
Micah Fitch <fitchmicah@gmail.com>
2018-12-17 23:21:54 -0600

    first commit... dunno if the cjs module actually works

---
+++ README.md
@@ -0,0 +1,11 @@
+# jest-html-differ
+
+TODO
+
+## Getting started
+
+TODO
+
+## License
+
+[MIT](LICENSE).
---
+++ dist/jest-html-differ.mjs
@@ -0,0 +1,37 @@
+import { toMatchSnapshot } from 'jest-snapshot';
+import HtmlDiffer from 'html-differ';
+import logger from 'html-differ/lib/logger';
+import expect from 'expect';
+
+function sameHtmlAs(a, b, options) {
+  const differ = new HtmlDiffer.HtmlDiffer(options);
+  let jestMsg;
+  try {
+    jestMsg = options.jestMsg;
+  } catch (e) {
+    jestMsg = diffLog => `HTML doesn't match:\n${diffLog}`;
+  }
+  const diff = differ.diffHtml(a, b);
+  const pass = diff.length === 1;
+  return {
+    pass,
+    message: () => jestMsg(logger.getDiffText(diff)),
+  };
+}
+
+function sameHtmlAsSnapshot() {
+  const html = Array.prototype.shift.apply(arguments);
+  const snapshot = toMatchSnapshot.apply(this, arguments);
+  const jestMsg = diffLog => `${snapshot.message().split('\n')[2]}\n${diffLog}`;
+
+  const result = sameHtmlAs(html, snapshot.expected, { jestMsg });
+  return result;
+}
+
+
+expect.extend({
+  sameHtmlAs,
+  sameHtmlAsSnapshot,
+});
+
+export { sameHtmlAs, sameHtmlAsSnapshot };
